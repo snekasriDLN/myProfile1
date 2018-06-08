9d516646fccdfa4b68ab07a49919d186bcad7497
snekasriDLN <snekasri777@gmail.com>
2018-06-09 04:37:48 +0530

    Exception

---
+++ src/com/mine/Exception.java
@@ -0,0 +1,12 @@
+package com.mine;
+
+public class Exception {
+	public static void main(String[] args) {
+try {
+		int num[] = {1, 2, 3, 4};
+    System.out.println(num[5]);
+}
+catch (ArrayIndexOutOfBoundsException e) {
+	System.out.println("Exception abc:"+e);
+}
+}}
